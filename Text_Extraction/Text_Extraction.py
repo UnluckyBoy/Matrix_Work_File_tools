871945cb57504c36414697c1301175e418a9bc4c
UnluckyBoy <qfmy_zhang@163.com>
2022-12-28 20:47:11 +0800

    Push By Matrix

--- Text_Extraction/Text_Extraction.py
+++ Text_Extraction/Text_Extraction.py
@@ -9,20 +9,20 @@ import argparse
 import pytesseract
 from PIL import Image
 
-def Text_Extract(args):
+def Text_Extract(path):
     # image=Image.open("1.jpg")
-    image = Image.open('./data/1.jpg')
+    image = Image.open(path)
+    # image = Image.open('./data/1.jpg')
     text = pytesseract.image_to_string(image, lang='chi_sim')  # 使用简体中文解析图片
     print(text)
     pass
 
 def main(args):
-    Text_Extract(args)
+    Text_Extract(args.image_path)
     pass
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
-    parser.add_argument('--image_path',type=str,default='./data/01.jpg',help='文件地址')
-    # parser.add_argument('--word_path',type=str,default='./data/demo.docx',help='word文件地址')
+    parser.add_argument('--image_path',type=str,default='./data/1.png',help='文件地址')
     args=parser.parse_args()
     main(args)
